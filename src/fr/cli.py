43a2b47b542a7100ff662ffe7de9248909f01239
sooj1n <sujin19371@gmail.com>
2024-09-10 16:07:57 +0900

    init

---
+++ src/fr/cli.py
@@ -0,0 +1,11 @@
+
+def predict():
+    length = float(input("물고기의 무게를 입력하세요: "))
+
+    ## weight 예측 선형회귀 API 호출
+    weight = lr_api(length)
+
+    ## 물고기 분류 API 호출
+    fish_class = knn_api(length, weight)
+
+    print(f"🐟 length:{length} 물고기는 weight:{weight} 으로 예측 되며 종류는 {fish_class} 입니다")
